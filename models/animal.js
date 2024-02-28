a036ee58b1a6743d6cea8f6bc8da25664e86d77c
unknown <liliannekho@gmail.com>
2024-02-28 10:57:08 -0500

    animal and user model created

---
+++ models/animal.js
@@ -0,0 +1,12 @@
+const { model, Schema } = require('mongoose')
+
+
+const animalSchema = new Schema ({
+    title: String,
+    body: String,
+    user: { type: Schema.Types.ObjectId, ref: 'User'}
+}, {
+    timestamps: true
+})
+
+module.exports = model('Animal', animalSchema)
